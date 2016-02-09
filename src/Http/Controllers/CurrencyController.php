d70ca6ee059e6f547c42cdc3cf831351c0e7e20b
Rolice <l.gardev@intellisys.org>
2016-02-09 12:50:42 +0200

    Improved currency controller.

--- src/Http/Controllers/CurrencyController.php
+++ src/Http/Controllers/CurrencyController.php
@@ -2,7 +2,9 @@
 namespace Rolice\LaravelCurrency\Http\Controllers;
 
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Lang;
 use Illuminate\Support\Facades\Input;
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Validator;
 use Rolice\LaravelCurrency\Exceptions\Exception;
 use Rolice\LaravelCurrency\Models\Currency;
@@ -28,25 +30,27 @@ class CurrencyController extends Controller
 
         $from = Currency::find(Input::get('from'));
         $to = Currency::find(Input::get('to'));
+        $precision = pow(10, (int) Config::get('laravel-currency.laravel-currency.precision'));
         $amount = (float)Input::get('amount') ?: 1;
+        $amount = round($amount * $precision);
+
+        if ($from && ($from->base == Input::get('to') || $from->base === $to->code)) {
+            return round(($amount / $from->price) / $precision, 2);
+        }
+
+        if ($to && ($to->base == Input::get('from') || $to->base = $from->code)) {
+            return round($amount * $to->price / $precision, 2);
+        }
 
         if (!$from || !$to) {
-            return response()->json(Lang::get('currency.error.not_found'))->setStatusCode(404, 'Currency Not Found');
-        }
-
-        if ($from->base === $to->code) {
-            return $amount / $from->price;
-        }
-
-        if ($to->base = $from->code) {
-            return $amount * $from->price;
+            return response()->json(['error' => Lang::get('currency.error.not_found')])->setStatusCode(404, 'Currency Not Found');
         }
 
         if ($from->base !== $to->base) {
             throw new Exception('Cannot convert currencies with different bases.');
         }
 
-        return ($amount / $from->price) * $to->price;
+        return ($amount / $from->price) * $to->price  / $precision;
     }
 
 }
